ff25e29df33197e1d78714a04e434720234537ef
ARGA\argak <argakusuma575@gmail.com>
2026-05-12 11:34:45 +0700

    Add [Arga] : add apk auto updater

--- app/src/main/java/com/example/personeltracking2026/data/model/UpdateApkResponse.kt
+++ app/src/main/java/com/example/personeltracking2026/data/model/UpdateApkResponse.kt
@@ -1,2 +1,8 @@
 package com.example.personeltracking2026.data.model
 
+data class UpdateApkResponse(
+    val versionCode: Int,
+    val versionName: String,
+    val apkUrl: String,
+    val changelog: String
+)
