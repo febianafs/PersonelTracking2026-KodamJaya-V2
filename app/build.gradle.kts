a65b70bae2b44cca9fa859a7e2133f7af6d7e3c5
ARGA\argak <argakusuma575@gmail.com>
2026-06-07 15:47:13 +0700

    no message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,9 @@ android {
         minSdk = 24
         targetSdk = 36
 
-        versionCode = 4
-        versionName = "1.0.3"
-        buildConfigField("String", "APP_VERSION", "\"1.0.3\"")
+        versionCode = 5
+        versionName = "1.0.4"
+        buildConfigField("String", "APP_VERSION", "\"1.0.4\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
